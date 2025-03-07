2346813c8aa0c603a2f4f9d67a78c2f593353240
Hleb2hek <156100937+Hleb2hek@users.noreply.github.com>
2025-03-07 09:54:07 +0300

    Add files via upload

--- script/slider.js
+++ script/slider.js
@@ -1,11 +1,24 @@
+// Интегрировал библиотеку Swiper
+
+
 new Swiper('.reviews__container', {
-	// Navigation arrows
+	/* ключ, который отключает с
+	помощью мышки симуляции
+	телефонного скролла
+	сделанно для того, чтобы в комьютерной версии
+	копировать текст
+	*/
 	simulateTouch: false,
+	// расстояние между слайдами
 	spaceBetween: 24,
+	// Кнопки
 	navigation: {
 		nextEl: '.reviews__btn-next',
 		prevEl: '.reviews__btn-prev ',
 	},
+	/* На каком масштабе экрана
+	менять кол-во слайдов
+	*/
 	breakpoints: {
 		570: {
 			slidesPerView: 1,
--- script/targetNav.js
+++ script/targetNav.js
@@ -1,4 +1,8 @@
-var controller = new ScrollMagic.Controller({globalSceneOptions: {duration: 100}});
+/* для эксперемента интегрировал библиотеку ScrollMagic
+когда переходите по якорю, то кнопка навигации меняет стиль, в моём
+случае она для того, чтобы показать на какой секции находимся
+*/
+let controller = new ScrollMagic.Controller({globalSceneOptions: {duration: 100}});
 new ScrollMagic.Scene({triggerElement: "#calculator"})
 		.setClassToggle("#link-1", "focus") // add class toggle
 		// .addIndicators() // add indicators (requires plugin)
